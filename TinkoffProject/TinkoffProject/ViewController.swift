682a797a6537ec310420ad5fb0dde0ef4301f91f
Gusarowa <elengusarowa@yandex.ru>
2023-03-16 22:17:50 +0300

    init project

---
+++ TinkoffProject/TinkoffProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffProject
+//
+//  Created by Лена Гусарова on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
